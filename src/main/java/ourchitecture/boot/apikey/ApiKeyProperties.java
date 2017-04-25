a1abc4321666c2e28f2467df0cb63350b79c9a54
Eric Swanson <iseric@gmail.com>
2017-04-25 13:42:51 -0400

    add javadoc comments to properties Trying to get YAML files to recognize the properties in Eclipse (STS)

--- src/main/java/ourchitecture/boot/apikey/ApiKeyProperties.java
+++ src/main/java/ourchitecture/boot/apikey/ApiKeyProperties.java
@@ -9,18 +9,34 @@ public class ApiKeyProperties {
 
 	private String secret = "";
 
+	/**
+	* Returns if the API Key verification is enabled or disabled.
+	* @return boolean {@code true} if the API Key verification is enabled; otherwise {@code false}.
+	*/
 	public boolean isEnabled() {
 		return this.enabled;
 	}
 
+	/**
+	* Enables or disables API Key verification.
+	* @param enabled {@code true} if verification is enabled; otherwise {@code false}.
+	*/
 	public void setEnabled(boolean enabled) {
 		this.enabled = enabled;
 	}
 
+	/**
+	* Returns the API Key secret.
+	* @return String The API Key secret.
+	*/
 	public String getSecret() {
 		return this.secret;
 	}
 
+	/**
+	* Sets the API Key secret.
+	* @param secret The API Key secret.
+	*/
 	public void setSecret(String secret) {
 		this.secret = secret;
 	}
